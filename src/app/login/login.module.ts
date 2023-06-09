86f9ccd3d7ec0fff1842df7e8ca0221117d98d96
Kevin-Galarza77 <jimenezkev1040@gmail.com~>
2023-06-09 15:01:02 -0500

    Login listo

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 import { IonicModule } from '@ionic/angular';
@@ -8,6 +8,10 @@ import { IonicModule } from '@ionic/angular';
 import { LoginPageRoutingModule } from './login-routing.module';
 
 import { LoginPage } from './login.page';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @NgModule({
   imports: [
@@ -15,7 +19,12 @@ import { LoginPage } from './login.page';
     FormsModule,
     IonicModule,
     LoginPageRoutingModule,
-    MatSlideToggleModule
+    MatSlideToggleModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    ReactiveFormsModule
   ],
   declarations: [LoginPage]
 })
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AlertController, LoadingController } from '@ionic/angular';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  hide = true;
+
+  loginForm: FormGroup = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]]
+  });
+
+
+  constructor(private fb: FormBuilder,private loadingController: LoadingController,private alertController: AlertController,private authService: AuthService,private router: Router) {
+
+   }
 
   ngOnInit() {
+    
+  }
+
+  async login(){
+    if (this.loginForm.valid) {
+      const loading = await this.loadingController.create();
+      await loading.present();
+  
+      const user = await this.authService.login(this.loginForm.value);
+      await loading.dismiss();
+  
+      if (user) {
+        this.router.navigateByUrl('/home', { replaceUrl: true });
+      } else {
+        this.showAlert('Contraseña o email incorrectos!', 'Intenta de nuevo!');
+      }
+    }
+  }
+
+  async showAlert(header: any, message: any) {
+    const alert = await this.alertController.create({
+      header,
+      message,
+      buttons: ['OK']
+    });
+    await alert.present();
   }
 
 }
